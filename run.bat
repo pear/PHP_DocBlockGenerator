f6f4aa43c82e8f251fa2b5915d3dadfc2bea9d9a
James Wade <hm2k@php.net>
2011-09-06 14:45:44 +0000

    rm run.bat: surplus to requirement
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_DocBlockGenerator/trunk@316262 c90b9560-bf6c-de11-be94-00142212c4b1

--- run.bat
+++
@@ -1,4 +0,0 @@
-call pear uninstall  --ignore-errors PHP_DocBlockGenerator
-call pear package-validate package2.xml
-call pear package package2.xml
-call pear install PHP_DocBlockGenerator-1.1.1.tgz
