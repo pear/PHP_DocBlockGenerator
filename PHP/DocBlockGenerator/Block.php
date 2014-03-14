467bae208cad9883f83ca230093bc0cb7c4578b5
Christian Weiske <cweiske@cweiske.de>
2014-03-14 06:45:18 +0100

    set default VCS to empty (not generate @version in file docblocks

--- PHP/DocBlockGenerator/Block.php
+++ PHP/DocBlockGenerator/Block.php
@@ -208,7 +208,7 @@ class PHP_DocBlockGenerator_Block
         // defaults the copyright year to the current year
         $this->pageTagDefaults['year'] = date('Y');
         // sets the default version to CVS
-        $this->pageTagDefaults['version'] = 'cvs';
+        $this->pageTagDefaults['version'] = '';
         // captures the calling PHP_DocBlockGenerator_Tokens instance
         $this->tokens = $tokens;
     }
